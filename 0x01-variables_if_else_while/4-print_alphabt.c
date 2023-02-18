f88abfd8e6997c02daeceb9d2a914bf01192750b
faithleo8 <leofaith8@gmail.com>
2023-02-18 00:29:11 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function to print alphabet in lowercases but skip q and e
+ *
+ * Return : Always (0) success
+ */
+int main(void)
+{
+int x:
+
+for (x = 'a'; x <= 'z'; x++)
+if (x != 'e' && x != 'q')
+putchar(x);
+
+putchar('\n');
+
+return (0);
+}
