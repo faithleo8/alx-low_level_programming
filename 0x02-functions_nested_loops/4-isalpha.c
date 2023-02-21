30ad8b69f422ba32df800116bb9d79d69659ecee
faithleo8 <leofaith8@gmail.com>
2023-02-21 12:30:28 -0800

    task4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
  *
  * @c: the character to be checked
  *
- * REturn: 1 if c is a letter, o otherwise
+ * Return: 1 if c is a letter, o otherwise
  */
 int _isalpha(int c)
 {
-	return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
