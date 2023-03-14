3bd8f7918061db19c41aa31ebe345137c3e6eb88
faithleo8 <leofaith8@gmail.com>
2023-03-14 14:00:27 -0700

    task one

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
 /**
  * main - print the number of arguments passed to the program
  * @argc: number of arguments
- * @argv: array of arguments		
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
